fdde59b1767668e9fd36e2314641ed3f8277a708
sc690 <sc690@hmgcc.gov.uk>
2018-09-28 15:03:52 +0100

    Added calculate prototype| Added README.md

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace CalculatorNS
 {
     public class Calculator
     {
+        /// <summary>
+        /// Default constructor for the Calculator
+        /// </summary>
         public Calculator()
         {
 
         }
+
+        /// <summary>
+        /// Perform the calculation required by the user
+        /// </summary>
+        /// <param name="input">The string representation of the user
+        /// specified calculation</param>
+        /// <returns>The result of the calculation</returns>
+        public double Calculate(string input)
+        {
+            return 0;
+        }
+
+
     }
+
+
 }
